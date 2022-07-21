7e3d451e2022a2f06bd09685586107abf4ce4d5f
yskanbye <yassyok@ymail.ne.jp>
2022-07-21 11:15:27 +0900

    7/21 2th

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       //$this->call(TodolistTableSeeder::class);
-      $this->call(TaskTableSeeder::class);
+      $this->call(TasksTableSeeder::class);
     }
 }
--- database/seeders/TasksTableSeeder.php
+++ database/seeders/TasksTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Task;
 
 class TasksTableSeeder extends Seeder
 {
